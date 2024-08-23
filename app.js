ceb6ab9250b500eefc089947e9cfef73921c06c2
Anshul2202 <anshul220206@gmail.com>
2024-08-23 17:07:16 +0530

    added a lib

---
+++ app.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import quote from 'inspirational-quotes'
+
+// axios.get('https://api.thecatapi.com/v1/images/search')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+
+// const Quote = require('inspirational-quotes');
+
+// console.log(Quote.getQuote());
+// console.log(Quote.getQuote({ author: false })); // return quote without author
+// console.log(Quote.getRandomQuote()); 
